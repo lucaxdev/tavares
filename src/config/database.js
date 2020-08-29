1d22add711af104e0688902a347a943bd828f4e7
lucas cavalcante <lucas090163@gmail.com>
2020-08-28 22:17:48 -0400

    Updated Yarn lockfile

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
     dialect: 'mysql',
-    host:'mysql669.umbler.com:41890',
+    host:'mysql669.umbler.com',
+    port: '41890',
     username: 'cesar_filho',
     password:'3}79r,bgTfVQ',
     database: 'tavares_dados1',
